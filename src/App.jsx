b10f7439b8b123ea0d730eff146d55dd18a0bf78
Chakibceran22 <grabachakib008@gmail.com>
2025-02-03 22:42:12 +0100

    creating the landing page for the app

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import PasswordHasher from './componenets/PasswordHahsing'
 import PasswordDecryptor from './componenets/PasswordDecryption'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import SecurityLandingPage from './componenets/LandingPage'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    <>
-    <PasswordDecryptor/>
+
+    <Router>
+      <Routes>
+        <Route path='/' element={<SecurityLandingPage/>}></Route>
+        <Route path='/hash' element={<PasswordHasher/>}></Route>
+        <Route path='/decrypt' element={<PasswordDecryptor/>}></Route>
+      </Routes>
+    </Router>
    </>
   )
 }
