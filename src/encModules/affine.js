59d4146e397bb4ccbd10c9924801ef02ac6b7560
Chakibceran22 <grabachakib008@gmail.com>
2025-02-11 14:52:23 +0100

    implementing affine dec

--- src/encModules/affine.js
+++ src/encModules/affine.js
@@ -1,7 +1,12 @@
-import { mod } from "mathjs"
-
+import { gcd, mod } from "mathjs"
+const modInverse = (a, m) => {
+    //later i will develop this function
+}
 const encryptAffine = (plainText, a, b) => {
     let cipherText = '';
+    if(gcd(a, 26) !== 1){
+        return 'Invalid key for Affine Cipher gcd(a, 26) !== 1';
+    }
     for (let i = 0; i < plainText.length ; i++)
     {
         if( plainText[i].match(/[a-z]/i)) {
@@ -17,13 +22,15 @@ const encryptAffine = (plainText, a, b) => {
     return cipherText;
 }
 const decryptAffine = (cipherText, a, b) => {
+    if(gcd(a, 26) !== 1){
+        return 'Invalid key for Affine Cipher gcd(a, 26) !== 1';
+    }
     let plainText = '';
     for (let i = 0; i < cipherText.length ; i++)
     {
         if( cipherText[i].match(/[a-z]/i)) {
             const base = cipherText[i] <= 'Z' ? 65 : 97;
             const cNum = cipherText[i].charCodeAt(0) - base;
-            console.log(cNum)
             const pNum = mod(a * (cNum - b), 26);
             plainText += String.fromCharCode(pNum + base);
         }
