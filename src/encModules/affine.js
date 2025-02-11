5f417fe7d206156f95cab4891357148e1b24de04
Chakibceran22 <grabachakib008@gmail.com>
2025-02-11 14:06:44 +0100

    fisnign front issues and createing the affine js module

---
+++ src/encModules/affine.js
@@ -0,0 +1,39 @@
+import { mod } from "mathjs"
+import {modInv} from "mathjs"
+
+const encrypt = (plainText, a, b) => {
+    let cipherText = '';
+    for (let i = 0; i < plainText.length ; i++)
+    {
+        if( plainText[i].match(/[a-z]/i)) {
+            const base = plainText[i] <= 'Z' ? 65 : 97;
+            const pNum = plainText[i].charCodeAt(0) - base;
+            const cNum = mod(a * pNum + b, 26);
+            cipherText += String.fromCharCode(cNum + base);
+        }
+        else{
+            cipherText += plainText[i];
+        }
+    }
+    return cipherText;
+}
+const keya = 21
+const decrypt = (cipherText, a, b) => {
+    let plainText = '';
+    for (let i = 0; i < cipherText.length ; i++)
+    {
+        if( cipherText[i].match(/[a-z]/i)) {
+            const base = cipherText[i] <= 'Z' ? 65 : 97;
+            const cNum = cipherText[i].charCodeAt(0) - base;
+            console.log(cNum)
+            const pNum = mod(a * (cNum - b), 26);
+            plainText += String.fromCharCode(pNum + base);
+        }
+        else{
+            plainText += cipherText[i];
+        }
+    }
+    return plainText;
+}
+
+export { encrypt, decrypt }
