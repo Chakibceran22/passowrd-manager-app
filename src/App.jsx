2d503e7e008574f7749d41f068563b25faf8487f
Chakibceran22 <grabachakib008@gmail.com>
2025-02-27 20:59:38 +0100

    fixing some cleaning issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import PasswordHasher from './componenets/PasswordHahsing'
 import PasswordDecryptor from './pages/PasswordDecryption'
 import PasswordEncryptor from './pages/passwordEncryptionPage'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import SecurityLandingPage from './componenets/LandingPage'
+import SecurityLandingPage from './pages/LandingPage'
 import NotFound from './componenets/NotFoundPage'
 import CryptanalysisTools from './pages/FrequencyAnalysisPage'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
    <>
